e89479656b411cbce40950b026ccca9347a27ec7
macbook079 <clukethao@gmail.com>
2022-08-10 16:34:11 -0500

    added inventory entity functionality to list all items

--- backend/src/main/java/net/yorksolutions/backend/Inventory/InventoryRepository.java
+++ backend/src/main/java/net/yorksolutions/backend/Inventory/InventoryRepository.java
@@ -3,6 +3,10 @@ package net.yorksolutions.backend.Inventory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface InventoryRepository extends CrudRepository<Inventory, Long> {
+
+    ArrayList<Inventory> getAllBy();
 }
